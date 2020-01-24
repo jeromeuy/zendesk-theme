fe26fdd00e56f1a1a4bf27137ed27fc602da57fd
Cynthia Arty Ng <cng@gitlab.com>
2020-01-24 16:42:32 +0100

    Draft custom version

--- script.js
+++ script.js
@@ -1,75 +1,71 @@
-/*
- * jQuery v1.9.1 included
- */
+hljs.initHighlightingOnLoad();
 
-$(document).ready(function() {
+var HC_SETTINGS = {
+  css: {
+    activeClass: "is-active",
+    hiddenClass: "is-hidden"
+  }
+};
 
-  // social share popups
+var Utils = {
+  isHomepage: function() {
+    return $("[data-home-page]").length > 0;
+  }
+};
+
+$(function() {
+  var $topbar = $("[data-topbar]");
+  var $heroUnit = $("[data-hero-unit]");
+  var $topSearchBar = $(".topbar__search-bar");
+  var $topSearchBarQuery = $topSearchBar.find("#query");
+  var $topSearchBarBtn = $(".topbar__btn-search");
+
+  if (Utils.isHomepage()) {
+    $topbar.addClass("topbar--large");
+    $heroUnit.removeClass(HC_SETTINGS.css.hiddenClass);
+    $("[data-wave-large]").removeClass(HC_SETTINGS.css.hiddenClass);
+    $("[data-footer-submit-ticket]").removeClass(HC_SETTINGS.css.hiddenClass);
+  } else {
+    $topbar.addClass("topbar--small");
+    $("[data-wave-small]").removeClass(HC_SETTINGS.css.hiddenClass);
+  }
+
+  $topbar.removeClass(HC_SETTINGS.css.hiddenClass);
+
+  $("[data-toggle-menu]").click(function() {
+    $(this).toggleClass(HC_SETTINGS.css.activeClass);
+    $("[data-menu]").toggle();
+  });
+
+  // Social share popups
   $(".share a").click(function(e) {
     e.preventDefault();
     window.open(this.href, "", "height = 500, width = 500");
   });
 
-  // toggle the share dropdown in communities
+  // Toggle the share dropdown in communities
   $(".share-label").on("click", function(e) {
     e.stopPropagation();
-    var isSelected = this.getAttribute("aria-selected") == "true";
+    var isSelected = this.getAttribute("aria-selected") === "true";
     this.setAttribute("aria-selected", !isSelected);
-    $(".share-label").not(this).attr("aria-selected", "false");
+    $(".share-label")
+      .not(this)
+      .attr("aria-selected", "false");
   });
 
   $(document).on("click", function() {
     $(".share-label").attr("aria-selected", "false");
   });
 
-  // show form controls when the textarea receives focus or backbutton is used and value exists
-  var $commentContainerTextarea = $(".comment-container textarea"),
-      $commentContainerFormControls = $(".comment-form-controls");
-
-  $commentContainerTextarea.one("focus", function() {
-    $commentContainerFormControls.show();
-  });
-
-  if ($commentContainerTextarea.val() !== "") {
-    $commentContainerFormControls.show();
-  }
-
-  // Mark as solved button
-  var $requestMarkAsSolvedButton = $(".request-comment-form .comment-container .mark-as-solved:not([data-disabled])"),
-    $requestMarkAsSolvedCheckbox = $(".request-comment-form .comment-container input[type=checkbox]"),
-    $requestCommentSubmitButton = $(".request-comment-form .comment-container input[type=submit]");
-
-  $requestMarkAsSolvedButton.on("click", function () {
-    $requestMarkAsSolvedCheckbox.attr("checked", true);
-    $requestCommentSubmitButton.prop("disabled", true);
-    $(this).attr("data-disabled", true).closest("form").submit();
-  });
-
-  // Change Mark as solved text according to whether comment is filled
-  var $requestCommentTextarea = $(".request-comment-form .comment-container textarea");
-
-  $requestCommentTextarea.on("keyup", function() {
-    if ($requestCommentTextarea.val() !== "") {
-      $requestMarkAsSolvedButton.text($requestMarkAsSolvedButton.data("solve-and-submit-translation"));
-      $requestCommentSubmitButton.prop("disabled", false);
-    } else {
-      $requestMarkAsSolvedButton.text($requestMarkAsSolvedButton.data("solve-translation"));
-      $requestCommentSubmitButton.prop("disabled", true);
-    }
-  });
-
-  // Disable submit button if textarea is empty
-  if ($requestCommentTextarea.val() === "") {
-    $requestCommentSubmitButton.prop("disabled", true);
-  }
-
-  // Submit requests filter form in the request list page
-  $("#request-status-select, #request-organization-select")
-    .on("change", function() {
+  // Submit search on select change
+  $("#request-status-select, #request-organization-select").on(
+    "change",
+    function() {
       search();
-    });
+    }
+  );
 
-  // Submit requests filter form in the request list page
+  // Submit search on input enter
   $("#quick-search").on("keypress", function(e) {
     if (e.which === 13) {
       search();
@@ -84,10 +80,101 @@ $(document).ready(function() {
     });
   }
 
-  // Submit organization form in the request page
-  $("#request-organization select").on("change", function() {
-    this.form.submit();
+  $(".image-with-lightbox").magnificPopup({
+    type: "image",
+    closeOnContentClick: true,
+    closeBtnInside: false,
+    fixedContentPos: true,
+    mainClass: "mfp-with-zoom", // class to remove default margin from left and right side
+    image: {
+      verticalFit: true
+    },
+    zoom: {
+      enabled: true,
+      duration: 300 // don't foget to change the duration also in CSS
+    }
   });
 
-});
+  $(".image-with-video-icon").magnificPopup({
+    disableOn: 700,
+    type: "iframe",
+    mainClass: "mfp-fade",
+    removalDelay: 160,
+    preloader: false,
+    fixedContentPos: false
+  });
 
+  $(".accordion__item-title").on("click", function() {
+    var $title = $(this);
+    $title.toggleClass("accordion__item-title--active");
+    $title
+      .parents(".accordion__item")
+      .find(".accordion__item-content")
+      .slideToggle();
+  });
+
+  $(".tabs-link").click(function(e) {
+    e.preventDefault();
+    var $link = $(this);
+    var tabIndex = $link.index();
+    var $tab = $link
+      .parents(".tabs")
+      .find(".tab")
+      .eq(tabIndex);
+    $link
+      .addClass(HC_SETTINGS.css.activeClass)
+      .siblings()
+      .removeClass(HC_SETTINGS.css.activeClass);
+    $tab
+      .removeClass(HC_SETTINGS.css.hiddenClass)
+      .siblings(".tab")
+      .addClass(HC_SETTINGS.css.hiddenClass);
+  });
+
+  $topSearchBarBtn.click(function() {
+    $(this).addClass(HC_SETTINGS.css.hiddenClass);
+    $topSearchBar.removeClass(HC_SETTINGS.css.hiddenClass);
+    $topSearchBarQuery.focus();
+  });
+
+  $(document).mouseup(function(e) {
+    if (!$topSearchBarQuery.is(e.target)) {
+      $topSearchBar.addClass(HC_SETTINGS.css.hiddenClass);
+      $topSearchBarBtn.removeClass(HC_SETTINGS.css.hiddenClass);
+    }
+  });
+
+  // Fix animated icons
+  $(".fa-spin").empty();
+
+  $("img.custom-block__image").each(function() {
+    var $img = $(this);
+    var imgID = $img.attr("id");
+    var imgClass = $img.attr("class");
+    var imgURL = $img.attr("src") + "?reset";
+
+    $.get(
+      imgURL,
+      function(data) {
+        // Get the SVG tag, ignore the rest
+        var $svg = $(data).find("svg");
+
+        // Add replaced image's ID to the new SVG
+        if (typeof imgID !== "undefined") {
+          $svg = $svg.attr("id", imgID);
+        }
+        // Add replaced image's classes to the new SVG
+        if (typeof imgClass !== "undefined") {
+          $svg = $svg.attr("class", imgClass + " replaced-svg");
+        }
+
+        // Remove any invalid XML tags as per http://validator.w3.org
+        $svg = $svg.removeAttr("xmlns:a");
+
+        // Replace image with new SVG
+        $img.replaceWith($svg);
+      },
+      "xml"
+    );
+  });
+});
